7f8bd806e123e79114ce3efb990e690e2ce35a9e
Pamela passig <131300611+PamelaPassig@users.noreply.github.com>
2025-09-07 09:04:29 -0300

    Code pronto atividade6.2

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,19 @@ public class Main {
     public static void main(String[] args) {
         LivrosDAO dao = new LivrosDAO();
 
-        // Inserir livro
         Livro livro = new Livro("Dom Casmurro", "Machado de Assis", "Romance", 5);
         dao.inserir(livro);
         System.out.println("Livro inserido com sucesso!");
 
-        // Listar livros
         List<Livro> livros = dao.listar();
         for (Livro l : livros) {
             System.out.println(l.getId() + " - " + l.getTitulo() + " (" + l.getAutor() + ")");
+        }
+
+        if (!livros.isEmpty()) {
+            System.out.println("Teste OK: livros foram inseridos e listados!");
+        } else {
+            System.out.println("Teste FALHOU: lista de livros vazia.");
         }
     }
 }
